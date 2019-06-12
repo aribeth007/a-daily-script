13419f6868369b415e82e30c6122530c55920494
Alexandra Petre <alpetre@expedia.com>
2019-06-12 10:46:00 +0300

    script-utils-1 - git fixup 4 commits

---
+++ git-related/force_push_with_fixup.sh
@@ -0,0 +1,12 @@
+# checkout branch with name myBranch
+git checkout myBranch
+
+# rebase for the last 4 commits
+git rebase -i myBranch~4 myBranch
+
+# at the interactive screen
+
+# choose fixup for commit: 2 / 3 / 4
+
+# force push the squashed commits
+git push -u origin +myBranch
